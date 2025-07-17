1a8ba9cd6187c84b1605bdec58ee42c021e2d448
Yarov Rivera <yarovrr@gmail.com>
2025-07-17 16:23:40 -0600

    update deploy
    Signed-off-by: Yarov Rivera <yarovrr@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
