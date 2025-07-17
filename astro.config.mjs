064458488800942f3a550976e6386223f1c7d0b7
Yarov Rivera <yarovrr@gmail.com>
2025-07-17 16:29:40 -0600

    udpate
    Signed-off-by: Yarov Rivera <yarovrr@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  site: 'https://amaqueme.mx/',
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind()],
